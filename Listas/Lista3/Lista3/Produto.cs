2cc32e5f3c72b840e60b01e3ef91752aabd9e623
Lucas Ciziks <luciziks@hotmail.com>
2019-03-12 02:49:39 -0300

    Adding Exercise 3

---
+++ Listas/Lista3/Lista3/Produto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3
+{
+    class Produto
+    {
+        public string Nome
+        {
+            get; private set;
+        }
+        public int Codigo
+        {
+            get; private set;
+        }
+        public int Quantidade
+        {
+            get;set;
+        }
+
+        private double preco;
+        
+
+        public double Preco
+        {
+            get
+            {
+                return this.preco;
+            }
+
+            set
+            {
+
+                if (value > 0)
+                    this.preco = value;
+            }
+            
+        }
+        
+
+        public Produto(string Nome,int Codigo,int Quantidade, double Preco)
+        {
+            this.Nome = Nome;
+            this.Codigo = Codigo;
+            this.Quantidade = Quantidade;
+            this.preco = Preco;
+        }
+
+        public double ValorTotal()
+        {
+           return this.Preco * this.Quantidade;
+        }
+    }
+}
---
+++ Listas/Lista3/Lista3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Insira o nome do Produto:"); string nome = Console.ReadLine();
+            Console.WriteLine("Insira o código do Produto: ");int codigo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a Quantidade no Estoque: ");int quant = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o Preço do Produto: ");double preco = double.Parse(Console.ReadLine());
+
+            Produto p1 = new Produto(nome,codigo,quant,preco);
+            Console.WriteLine("-------------------------Produto---------------");
+            Console.WriteLine("Nome: "+p1.Nome);
+            Console.WriteLine("Codigo: " + p1.Codigo);
+            Console.WriteLine("Quantidade em estoque: " + p1.Quantidade);
+            Console.WriteLine("Preco: " + p1.Preco);
+            Console.WriteLine("Valor Total: " + p1.ValorTotal());
+            Console.WriteLine("-----------------------------------------------");
+
+        }
+
+        
+    }
+}
